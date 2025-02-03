fd0a5d2ab54bfb367b63436cb69264167c1a02e0
HerrNamenlos123 <herrnamenlos123@gmail.com>
2025-02-03 23:47:00 +0100

    new tokenizer

--- src/main.py
+++ src/main.py
@@ -1,16 +1,5 @@
 from enum import Enum
-
-ValidIdentifierStartChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_"
-ValidIdentifierChars = ValidIdentifierStartChars + "0123456789"
-ValidNumberChars = "0123456789"
-WhitespaceChars = " \n\r\t"
-PunctuationChars = "{}[]()<>.:;-,"
-
-class TokenType(Enum):
-    Identifier = 1
-    StringLiteral = 2
-    Number = 3
-    Punctuation = 4
+import re
 
 class Token:
     def __init__(self, text, type):
@@ -21,72 +10,127 @@ class Token:
     def __repr__(self):
         return f"'{self.text}'"
 
-def tokenize(file: str):
-    tokens: List[Token] = []
+def tokenize(file: str, matchers):
+  tokens: List[Token] = []
+  
+  while len(file) > 0:
+    matched = False
+    for matcher in matchers:
+      match = re.match(matcher["matcher"], file)
+      if match:
+        span = match.span()
+        if span == (-1,-1) or span[0] != 0:
+          raise Exception(f"token match has invalid span at '{file[:10]}'")
+        
+        text = file[0:span[1]]
+        file = file[span[1]:]
+        matched = True
 
-    token = None
+        if matcher["type"] is None:
+          break # ignore whitespace
+  
+        if "valueExtractor" in matcher:
+          text = matcher["valueExtractor"](text)
 
-    for c in file:
-        if not token:
-            if c in ValidIdentifierStartChars:
-                token = Token(c, TokenType.Identifier)
-            elif c in WhitespaceChars:
-                pass
-            elif c == "\"":
-                token = Token(c, TokenType.StringLiteral)
-            elif c in PunctuationChars:
-                tokens.append(Token(c, TokenType.Punctuation))
-            elif c in ValidNumberChars:
-                token = Token(c, TokenType.Number)
-            else:
-                raise Exception(f"Unexpected token in string literal: '{c}' ({ord(c)})")
-        elif token.type is TokenType.Number:
-            if c in ValidNumberChars or c == ".":
-                token.text += c
-            elif c in WhitespaceChars:
-                tokens.append(token)
-                token = None
-            elif c in PunctuationChars:
-                tokens.append(token)
-                tokens.append(Token(c, TokenType.Punctuation))
-                token = None
-            else:
-                raise Exception(f"Unexpected token in number: '{c}' ({ord(c)})")
-        elif token.type is TokenType.Identifier:
-            if c in ValidIdentifierChars:
-                token.text += c
-            elif c in WhitespaceChars:
-                tokens.append(token)
-                token = None
-            elif c == "\"":
-                tokens.append(token)
-                token = Token("\"" + c, TokenType.StringLiteral)
-            elif c in PunctuationChars:
-                tokens.append(token)
-                tokens.append(Token(c, TokenType.Punctuation))
-                token = None
-            else:
-                raise Exception(f"Unexpected token in identifier: '{c}' ({ord(c)})")
-        elif token.type is TokenType.StringLiteral:
-            if c == "\"":
-                token.text += "\""
-                tokens.append(token)
-                token = None
-            else:
-                token.text += c
-        else:
-            raise Exception(f"Internal Error: Invalid tokenizer state")
-
-    if token:
-        tokens.append(token)
-
-    print("Tokens: ")
-    print(tokens)
+        tokens.append(Token(text, matcher["type"]))
+        # print(f"Matched token '{text}'")
+        break
+    
+    if not matched:
+      raise Exception(f"Unexpected token at '{file[:10]}'")
 
 def main():
-    with open("example.hz") as f:
-        file = f.read()
-        tokenize(file)
+  with open("example.hz") as f:
+    file = f.read()
+    # tokenize(file)
+    matchers = [
+    # { "matcher": r"", "type": "", "valueExtractor": lambda },
+      { "matcher": r"[ \r\n\t]+", "type": None },
+    # { "matcher": r"\r?\n", "type": None },
+      { "matcher": r"\/\/(.*?)(?=\r?\n|$)", "type": "comment", "valueExtractor": lambda s: s[2:] },
+      { "matcher": r"import", "type": "import" },
+    # { "matcher": r"\/\/(.*?)(?=\r?\n|$)", "type": "comment", "valueExtractor": lambda s: s[1:-2] },
+      { "matcher": r"\"[^\"\r\n]+\"", "type": "string-literal", "valueExtractor": lambda s: s[1:-2] },
+      { "matcher": r"'[^'\r\n]+'", "type": "string-literal", "valueExtractor": lambda s: s[1:-2] },
+      { "matcher": r"-?[0-9]+\.?[0-9]*(?![a-zA-Z$_])", "type": "number-literal" },
+      { "matcher": r"{", "type": "{" },
+      { "matcher": r"}", "type": "}" },
+      { "matcher": r"\[", "type": "[" },
+      { "matcher": r"\]", "type": "]" },
+      { "matcher": r"\(", "type": "(" },
+      { "matcher": r"\)", "type": ")" },
+      { "matcher": r";", "type": ";" },
+      { "matcher": r":", "type": ":" },
+      { "matcher": r",", "type": "," },
+      { "matcher": r"\.\.\.", "type": "..." },
+      { "matcher": r"\.", "type": "." },
+      { "matcher": r"\*\*", "type": "**" },
+      { "matcher": r"\*", "type": "*" },
+      # { "matcher": r"===", "type": "===" },
+      { "matcher": r"==", "type": "==" },
+      { "matcher": r"=>", "type": "=>" },
+      { "matcher": r"=", "type": "=" },
+      # { "matcher": r"!==", "type": "!==" },
+      { "matcher": r"!=", "type": "!=" },
+      { "matcher": r"&&", "type": "&&" },
+      { "matcher": r"&", "type": "&" },
+      # { "matcher": r"\^", "type": "^" },
+      # { "matcher": r"~", "type": "~" },
+      { "matcher": r"!", "type": "!" },
+      { "matcher": r"\|\|", "type": "||" },
+      { "matcher": r"\|", "type": "|" },
+      { "matcher": r"\+\+", "type": "++" },
+      { "matcher": r"\+", "type": "+" },
+      { "matcher": r"\-\-", "type": "--" },
+      { "matcher": r"\-", "type": "-" },
+      # { "matcher": r"\\", "type": "\\" },
+      # { "matcher": r"%", "type": "%" },
+      # { "matcher": r"\?\?", "type": "??" },
+      # { "matcher": r"\?", "type": "?" },
+      { "matcher": r">=", "type": ">=" },
+      { "matcher": r"<=", "type": "<=" },
+      # { "matcher": r">>", "type": ">>" },
+      { "matcher": r">", "type": ">" },
+      # { "matcher": r"<<", "type": "<<" },
+      { "matcher": r"<", "type": "<" },
+      # { "matcher": r"null", "type": "null" },
+      { "matcher": r"none", "type": "none" },
+      { "matcher": r"unknown", "type": "unknown" },
+      { "matcher": r"true", "type": "true", "valueExtractor": lambda x: True },
+      { "matcher": r"false", "type": "false", "valueExtractor": lambda x: False },
+      { "matcher": r"import", "type": "import" },
+      { "matcher": r"export", "type": "export" },
+      { "matcher": r"from", "type": "from" },
+      { "matcher": r"as", "type": "as" },
+      { "matcher": r"for", "type": "for" },
+      { "matcher": r"while", "type": "while" },
+      # { "matcher": r"in", "type": "in" },
+      # { "matcher": r"of", "type": "of" },
+      { "matcher": r"break", "type": "break" },
+      { "matcher": r"continue", "type": "continue" },
+      { "matcher": r"do", "type": "do" },
+      { "matcher": r"if", "type": "if" },
+      { "matcher": r"else", "type": "else" },
+      { "matcher": r"switch", "type": "switch" },
+      { "matcher": r"case", "type": "case" },
+      # { "matcher": r"default", "type": "default" },
+      # { "matcher": r"function", "type": "function" },
+      { "matcher": r"return", "type": "return" },
+      # { "matcher": r"yield", "type": "yield" },
+      # { "matcher": r"await", "type": "await" },
+      # { "matcher": r"try", "type": "try" },
+      # { "matcher": r"catch", "type": "catch" },
+      # { "matcher": r"finally", "type": "finally" },
+      # { "matcher": r"throw", "type": "throw" },
+      { "matcher": r"new", "type": "new" },
+      { "matcher": r"class", "type": "class" },
+      # { "matcher": r"super", "type": "super" },
+      { "matcher": r"let", "type": "let" },
+      { "matcher": r"const", "type": "const" },
+      { "matcher": r"this", "type": "this" },
+      { "matcher": r"[a-zA-Z$_][a-zA-Z0-9$_]*", "type": "identifier", "valueExtractor": lambda x: x },
+    ]
+    tokenize(file, matchers)
 
 if __name__ == "__main__":
     main()
