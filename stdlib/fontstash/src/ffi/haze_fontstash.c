092cf2882a920ad37f5673f93fa4016454fb8cec
HerrNamenlos123 <HerrNamenlos123@gmail.com>
2026-03-17 13:33:03 +0100

    small fixes

--- stdlib/fontstash/src/ffi/haze_fontstash.c
+++ stdlib/fontstash/src/ffi/haze_fontstash.c
@@ -6,13 +6,16 @@
 #include <stdlib.h>
 #include <string.h>
 
+#ifdef HAZE_PLATFORM_WIN32
+#include <windows.h>
+#endif
+
 #define FONTSTASH_IMPLEMENTATION
 #include "fontstash.h"
 
 #include "public/haze_fontstash.h"
 
-void* haze_fontstash_create(hzstd_int_t width, hzstd_int_t height)
-{
+void *haze_fontstash_create(hzstd_int_t width, hzstd_int_t height) {
   FONSparams params;
   memset(&params, 0, sizeof(params));
 
@@ -20,7 +23,7 @@ void* haze_fontstash_create(hzstd_int_t width, hzstd_int_t height)
   params.height = height;
   params.flags = FONS_ZERO_TOPLEFT;
 
-  FONScontext* fs = fonsCreateInternal(&params);
+  FONScontext *fs = fonsCreateInternal(&params);
   if (!fs) {
     return NULL;
   }
@@ -28,33 +31,28 @@ void* haze_fontstash_create(hzstd_int_t width, hzstd_int_t height)
   return fs;
 }
 
-void haze_fontstash_destroy(void* ctx)
-{
+void haze_fontstash_destroy(void *ctx) {
   assert(ctx);
-  fonsDeleteInternal((FONScontext*)ctx);
+  fonsDeleteInternal((FONScontext *)ctx);
 }
 
-hzstd_int_t haze_fontstash_add_font(void* ctx, hzstd_str_t name, hzstd_str_t path)
-{
+hzstd_int_t haze_fontstash_add_font(void *ctx, hzstd_str_t name,
+                                    hzstd_str_t path) {
   assert(ctx);
-  return fonsAddFont((FONScontext*)ctx, HZSTD_CSTR(name), HZSTD_CSTR(path));
+  return fonsAddFont((FONScontext *)ctx, HZSTD_CSTR(name), HZSTD_CSTR(path));
 }
 
-hzstd_int_t haze_fontstash_add_font_from_memory(void* ctx, hzstd_str_t name, hzstd_cptr_t data, hzstd_int_t length)
-{
+hzstd_int_t haze_fontstash_add_font_from_memory(void *ctx, hzstd_str_t name,
+                                                hzstd_cptr_t data,
+                                                hzstd_int_t length) {
   assert(ctx);
-  return fonsAddFontMem((FONScontext*)ctx, HZSTD_CSTR(name), data, length, 0);
+  return fonsAddFontMem((FONScontext *)ctx, HZSTD_CSTR(name), data, length, 0);
 }
 
-void haze_fontstash_layout_text(void* ctx,
-                                hzstd_int_t font,
-                                float size,
-                                float x,
-                                float y,
-                                hzstd_str_t text,
-                                hzstd_dynamic_array_t* out)
-{
-  FONScontext* fs = (FONScontext*)ctx;
+void haze_fontstash_layout_text(void *ctx, hzstd_int_t font, float size,
+                                float x, float y, hzstd_str_t text,
+                                hzstd_dynamic_array_t *out) {
+  FONScontext *fs = (FONScontext *)ctx;
 
   fonsSetFont(fs, font);
   fonsSetSize(fs, size);
@@ -66,36 +64,37 @@ void haze_fontstash_layout_text(void* ctx,
 
   while (fonsTextIterNext(fs, &iter, &quad)) {
     haze_fontstash_glyph_t glyph = {
-      .x0 = quad.x0,
-      .y0 = quad.y0,
-      .x1 = quad.x1,
-      .y1 = quad.y1,
+        .x0 = quad.x0,
+        .y0 = quad.y0,
+        .x1 = quad.x1,
+        .y1 = quad.y1,
 
-      .s0 = quad.s0,
-      .t0 = quad.t0,
-      .s1 = quad.s1,
-      .t1 = quad.t1,
+        .s0 = quad.s0,
+        .t0 = quad.t0,
+        .s1 = quad.s1,
+        .t1 = quad.t1,
     };
     HZSTD_DYNAMIC_ARRAY_PUSH(out, glyph);
   }
 }
 
-haze_fontstash_atlas_t haze_fontstash_get_atlas(void* ctx)
-{
+haze_fontstash_atlas_t haze_fontstash_get_atlas(void *ctx) {
   int w, hgt;
-  const unsigned char* pixels = fonsGetTextureData((FONScontext*)ctx, &w, &hgt);
+  const unsigned char *pixels =
+      fonsGetTextureData((FONScontext *)ctx, &w, &hgt);
 
   haze_fontstash_atlas_t atlas;
-  atlas.pixels = (void*)pixels;
+  atlas.pixels = (void *)pixels;
   atlas.width = w;
   atlas.height = hgt;
 
   return atlas;
 }
 
-haze_fontstash_metrics_t haze_fontstash_get_metrics(hzstd_cptr_t ctx, hzstd_int_t font, hzstd_real_t size)
-{
-  FONScontext* fs = ctx;
+haze_fontstash_metrics_t haze_fontstash_get_metrics(hzstd_cptr_t ctx,
+                                                    hzstd_int_t font,
+                                                    hzstd_real_t size) {
+  FONScontext *fs = ctx;
 
   fonsSetFont(fs, font);
   fonsSetSize(fs, size);
